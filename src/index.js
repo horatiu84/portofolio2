9ca74f20c727505168cbb6e43dd0038a287f82b4
horatiu84 <horatiul@yahoo.com>
2023-02-07 07:36:37 +0200

    made setup for the app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import './index.css';
+
+import App from "./App";
+
+const root = document.getElementById('root');
+const el = ReactDOM.createRoot(root);
+
+el.render(<App />)
